30a2de14d13751474241519d98c588fd2a0665b6
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-01-13 18:19:15 +0000

    Bug fixes.

--- src/Zeao/Main.php
+++ src/Zeao/Main.php
@@ -1,10 +1,14 @@
 <?php
+
 namespace Zeao;
+
 use Zeao\commands\TPToggleCommand;
 use Zeao\commands\TPto2Command;
 use Zeao\util\TeleportManager;
 use pocketmine\plugin\PluginBase;
+
 class Main extends PluginBase {
+    
     private static $plugin;
     protected $teleportManager;
     public function onEnable() {
@@ -13,16 +17,8 @@ class Main extends PluginBase {
         $this->teleportManager = new TeleportManager($this);
         $this->getCommand('tpto')->setExecutor(new TptoCommand($this));
         $this->getCommand('tptoggle')->setExecutor(new TpToggleCommand($this));
-    }
-    public function onDisable() {
-        # Cleanup in case of a reload.
-        unset($this->teleportManager,
-        self::$plugin = null;
-    }
-    public static function getPlugin(): TPToggle {
-        return self::$plugin;
-    }
-    public function getTeleportManager(): TeleportManager {
-        return $this->teleportManager;
-    }
+   }
+   public function onDisable() {
+        $this->getLogger()->info(TextFormat::RED . "TpToggle disabled.");
+   }
 }
